bff9d59c61da16852ef23fa804f5cda4db3b2b7a
TM <tmangeard@gmx.fr>
2019-04-01 20:28:26 +1100

    add mkdir to file .bashrc

--- vwrapper.sh
+++ vwrapper.sh
@@ -3,11 +3,11 @@ pip install virtualenvwrapper
 
 # Config à ajouter dans ~/.bashrc ou ~/.profile
 # - dossier contenant les environnements virtuels
-echo "export WORKON_HOME=~/.virtualenvs" >> ~/bashrc
-mkdir -p $WORKON_HOME
+echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
+echo "mkdir -p ~/.virtualenvs" >> ~/.bashrc
 # - dossier contenant les projets associés
-echo "export PROJECT_HOME=~/pyprojects" >> ~/bashrc
-mkdir -p $PROJECT_HOME
+echo "export PROJECT_HOME=~/pyprojects" >> ~/.bashrc
+echo "mkdir -p ~/pyprojects" >> ~/.bashrc
 # - s'assurer que virtualenvwrapper est toujours disponible
 source /usr/local/bin/virtualenvwrapper.sh
 
