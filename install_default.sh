8dfc299a1110e6a99a542fdab05954e920a91661
TM <tmangeard@gmx.fr>
2019-04-03 10:14:36 +1100

    add filezilla

--- install_default.sh
+++ install_default.sh
@@ -1,2 +1,2 @@
-sudo apt -y install curl git 
-sudo apt -y install python3 pip3 pylint flake8
+sudo apt -y install curl git filezilla htop
+sudo apt -y install python3-pip python-pip pyenv python3-venv pylint flake8
