a8b3299c7e87e41d35506e1541aef42c99402336
TM <tmangeard@gmx.fr>
2019-04-03 10:11:50 +1100

    add comment

--- gdal.sh
+++ gdal.sh
@@ -1,4 +1,4 @@
 sudo apt -y install gdal-bin libgdal-dev
-
+#sudo apt -y install libgdal-dev gdal-bin python-gdal libgdal1h
 #python gdal environement
 pip install --global-option=build_ext --global-option="-I/usr/include/gdal" GDAL==`gdal-config --version`
