7e29e55ced77124a0b480cc13c61707ec789a1e6
[Bird] <tmangeard@gmx.fr>
2019-12-20 11:00:24 +1100

    Update vwrapper.sh

--- vwrapper.sh
+++ vwrapper.sh
@@ -1,7 +1,7 @@
 # Installation
 
 pip install virtualenvwrapper
-echo "VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6" >> ~/.bashrc
+echo "VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
 # Config à ajouter dans ~/.bashrc ou ~/.profile
 # - dossier contenant les environnements virtuels
 echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
